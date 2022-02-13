47d7b65fbdc309b181944861bcb047da56139d4a
fentybit <fentymhall@gmail.com>
2022-02-13 14:44:31 -0800

    add applyMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -22,10 +22,10 @@ const rootReducer = combineReducers({
 
 const store = createStore(
     rootReducer,
-    // compose(
-    //     applyMiddleware(thunk),
-    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    // )
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
 );
 
 export default store;
